3a9cf05e9972f73f88edd30ebcf335147eaa06b4
jmhs11 <josemariahs@gmail.com>
2021-07-06 21:24:39 +0200

    New changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BodyComponent } from './components/body/body.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { NgClassComponent } from './components/ng-class/ng-class.component';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NgIfComponent } from './components/ng-if/ng-if.component';
+import { NgForComponent } from './components/ng-for/ng-for.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    BodyComponent,
+    NgStyleComponent,
+    NgClassComponent,
+    NgSwitchComponent,
+    FooterComponent,
+    NgIfComponent,
+    NgForComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
